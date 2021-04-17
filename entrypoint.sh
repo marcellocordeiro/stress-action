2aae8d904ee049f41a1913cf5ddac47cb806cca1
Marcello Cordeiro <msc3@cin.ufpe.br>
2021-04-17 18:59:16 -0300

    git clone --depth 1

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-git clone https://github.com/marcellocordeiro/stress-script.git ./__stress_script
+git clone --depth 1 https://github.com/marcellocordeiro/stress-script.git ./__stress_script
 python3 "./__stress_script/main.py" $INPUT_TEST_TYPE "." -o "./__stress_output" -nsr $INPUT_NO_STRESS_RUNS -sr $INPUT_STRESS_RUNS
